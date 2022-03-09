119b1e7e2402f9e999ee47de733622cffca577a5
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-03-09 08:18:11 +0100

    search word to loever case

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -105,7 +105,7 @@ const fetchCaption = async () => {
   
     datas = captions.filter(caption => {
         if(caption.text !== undefined){
-         return caption.text.includes(word)
+         return caption.text.includes(word.toLowerCase())
         }  
       })
   
